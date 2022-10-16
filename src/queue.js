ace9fc0dd6d5b71f7fda4b753df89d018d354e95
Pavel Didenko <win4ester2016@yandex.ru>
2022-10-16 19:04:19 +0300

    feat: add queue data structure

--- src/queue.js
+++ src/queue.js
@@ -13,22 +13,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+ class Node {
+  constructor(value) {
+      this.value = value;
+      this.next = null;
+  }
+}
+
+
 class Queue {
 
+  firstNode 
+  currentNode 
+  
+  
+
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // throw new NotImplementedError('Not implemented');
+    return this.firstNode
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    if(!this.firstNode){
+      this.firstNode = new Node(value)
+      this.currentNode = this.firstNode;
+    }else if(!this.firstNode.next){
+      this.firstNode.next = new Node(value)
+      this.currentNode = this.firstNode.next;
+    }else{
+      this.currentNode.next = new Node(value);
+      this.currentNode = this.currentNode.next;
+    }
   }
+
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.firstNode){
+      let tempValue = this.firstNode.value;
+      this.firstNode = this.firstNode.next;
+      return tempValue;
+    }else{
+       return null;
+    }
   }
+    
 }
 
 module.exports = {
